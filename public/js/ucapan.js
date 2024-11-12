5e8c3c24e8dff6d3b3953d8b2648ce7ddbbef40f
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2024-11-12 14:41:36 +0700

    fix store ucapan

--- public/js/ucapan.js
+++ public/js/ucapan.js
@@ -4,6 +4,8 @@ import {
   getFirestore,
   collection,
   getDocs,
+  addDoc,
+  serverTimestamp,
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
@@ -29,7 +31,7 @@ const getDataUcapan = async () => {
   let total_hadir = 0;
   let row_ucapan = "";
   querySnapshot.forEach((doc) => {
-    if (doc.data().kehadiran) total_hadir++;
+    if (doc.data().kehadiran == 1) total_hadir++;
     const createdAt = doc.data().created_at;
     let formattedDate = createdAt;
 
@@ -56,8 +58,88 @@ const getDataUcapan = async () => {
         </div>
     </div>`;
   });
-  $("#box_ucapan").append(row_ucapan);
+  $("#wrapper-ucapan").html(row_ucapan);
   $("#total-hadir-ucapan").text(total_hadir);
 };
 
 getDataUcapan();
+
+const storeUcapan = async (nama, ucapan, kehadiran) => {
+  try {
+    // Menyimpan data baru ke koleksi "ucapan"
+    const docRef = await addDoc(collection(db, "ucapan"), {
+      nama: nama,
+      ucapan: ucapan,
+      kehadiran: kehadiran,
+      created_at: serverTimestamp(), // Timestamp Firestore sebagai waktu penyimpanan
+    });
+    if (kehadiran == 1) {
+      Swal.fire({
+        imageUrl: './images/happy.png',
+        title: "Terima kasih ucapannya!",
+        text: "Kami tunggu kehadiranmu ya " + nama,
+      });
+    } else {
+      Swal.fire({
+        imageUrl: './images/sad.png',
+        title: "Terima kasih ucapannya!",
+        text: `Yahhh kamu ga hadir ya ${nama}... kami doakan kamu berubah pikiran untuk hadir yah.... hehe`,
+      });
+    }
+    $("#wrapper-ucapan")
+      .prepend(`<div class="kado_confirm_cl" style="margin-bottom: 10px; border-radius: 5px; ">
+        <div class="" style="font-weight: 800;">
+            <p class="ucapan-header" style="color: #2c2c2c !important; font-size: 14px !important;">${nama} - ${formatDate()}</p>
+            <i style="font-size: 10px; color: #2c2c2c !important">${ucapan}</i>
+        </div>
+    </div>`);
+    return true;
+  } catch (e) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Terjadi kesalahan saat menyimpan ucapan!",
+    });
+    return false;
+  }
+};
+
+const formatDate = () => {
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-GB"); // "en-GB" untuk format dd/mm/yyyy
+  const formattedTime = date.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return `${formattedDate} ${formattedTime}`;
+};
+
+$("#form_ucapan").submit(function (e) {
+  e.preventDefault();
+  const nama = $("[name='form_ucapan_nama']").val();
+  const ucapan = $("[name='form_ucapan_ucapan']").val();
+  const kehadiran = $("[name='form_ucapan_kehadiran']").val();
+  console.log(nama, ucapan, kehadiran);
+
+  if (nama === "" || ucapan.length < 1 || kehadiran === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Duhh...",
+      text: "Semua field harus diisi!",
+    });
+    return;
+  }
+  let resp = storeUcapan(nama, ucapan, kehadiran);
+  if (resp) {
+    $("[name='form_ucapan_nama']").val("");
+    $("[name='form_ucapan_ucapan']").val("");
+    $("[name='form_ucapan_kehadiran']").val("");
+    $("#total-ucapan-ucapan").text(
+      parseInt($("#total-ucapan-ucapan").text()) + 1
+    );
+    $("#total-hadir-ucapan").text(
+      parseInt($("#total-hadir-ucapan").text()) + (kehadiran == 1 ? 1 : 0)
+    );
+    openEditModal("ucapan-slide");
+  }
+});
